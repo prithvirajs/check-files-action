8eb9065af8ffd7eea99212023e34bee32d6893ec
prithviraj singh <prithviraj_mca@yahoo.co.in>
2023-08-10 19:17:42 +0530

    first actio ntest

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require("fs");
-const { connected } = require('process');
-const { Octokit } = require("@octokit/rest");
+const fs = require('fs');
 
-async function checkFileExistence(path) {
-    return fs.promises.access(path, fs.constants.F_OK)
+
+async function checkFileExists(filePath) {
+    return fs.promises.access(filePath)
     .then(() => {
-        core.info(`${path} exists`);
+        core.info(`File ${filePath} exists`);
         return true;
     })
     .catch(() => {
-        core.setFailed(`${path} does not exist`);
+        core.setFailed(`File ${filePath} is mandatory`);
         return false;
     });
-  }
-  // create a function that checks if the file starts with a markdown header
+}
+
+// create a function that checks if the file starts with a markdown header
 async function checkFileStartsWithHeader(filePath) {
     return fs.promises.readFile(filePath, 'utf8')
     .then(fileContent => {
@@ -33,50 +33,53 @@ async function checkFileStartsWithHeader(filePath) {
     });
 }
 
+(
+    async () => {
+        try {
+            checkFileExists("LICENSE");
+            checkFileExists("README.md");
 
-(async () => {
-    try {
-   
-        checkFileExistence("README.md");
-        checkFileExistence("LICENSE");
-        if (
-            ! await checkFileStartsWithHeader("README.md")
-        ) {
-            // get token for octokit
-            const token = core.getInput('repo-token');
-            const octokit = new github.getOctokit(token);
+            if (
+                ! await checkFileStartsWithHeader("README.md")
+            ) {
+                // get token for octokit
+                const token = core.getInput('repo-token');
+                const octokit = new github.getOctokit(token);
+
+                // call octokit to create a check with annotation and details
+                const check = await octokit.rest.checks.create({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    name: 'Readme Validator',
+                    head_sha: github.context.sha,
+                    status: 'completed',
+                    conclusion: 'failure',
+                    output: {
+                        title: 'README.md must start with a title',
+                        summary: 'Please use markdown syntax to create a title',
+                        annotations: [
+                            {
+
+                                path: 'README.md',
+                                start_line: 1,
+                                end_line: 1,
+                                annotation_level: 'failure',
+                                message: 'README.md must start with a header',
+                                start_column: 1,
+                                end_column: 1
+                            }
+                        ]
+                    }
+                });
 
 
-            // call octokit to create a check with annotation and details
-            const check = await octokit.rest.checks.create({
-                owner: github.context.repo.owner,
-                repo: github.context.repo.repo,
-                name: 'Readme Validator',
-                head_sha: github.context.sha,
-                status: 'completed',
-                conclusion: 'failure',
-                output: {
-                    title: 'README.md must start with a title',
-                    summary: 'Please use markdown syntax to create a title',
-                    annotations: [
-                        {
-                            path: 'README.md',
-                            start_line: 1,
-                            end_line: 1,
-                            annotation_level: 'failure',
-                            message: 'README.md must start with a header',
-                            start_column: 1,
-                            end_column: 1
-                        }
-                    ]
-                }
-            });
+
+            }
+            
+
+        } catch (error) {
+            core.setFailed(error.message);
         }
-
-        
-    } catch (error) {
-        core.setFailed(error.message);
     }
-})();
 
-
+)();
