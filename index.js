2d39d5ccf9140a1470d505b93ee2d1cd09549413
prithviraj singh <prithviraj_mca@yahoo.co.in>
2023-08-10 18:46:08 +0530

    first actio ntest

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function checkFileStartsWithHeader(filePath) {
             ! await checkFileStartsWithHeader("README.md")
         ) {
             // get token for octokit
-            const token = core.getInput('RTOKEN');
+            const token = core.getInput('repo-token');
             const octokit = new github.getOctokit(token);
 
 
